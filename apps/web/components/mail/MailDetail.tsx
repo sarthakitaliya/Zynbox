0aded3e151259be72102252a80db4219c698a047
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-06-05 12:14:05 +0530

    update email rendering

--- apps/web/components/mail/MailDetail.tsx
+++ apps/web/components/mail/MailDetail.tsx
@@ -1,19 +1,27 @@
-import { useEmailStore } from "@repo/store"
-
+import { useEmailStore } from "@repo/store";
 
 export const MailDetail = () => {
-    const { selectedEmail } = useEmailStore();
-    return(
-        <div className="flex-1 p-4">
-            {selectedEmail ? (
-                <div className="bg-zinc-800 rounded-lg p-4">
-                    <h2 className="text-white text-xl font-semibold mb-2">{selectedEmail.subject}</h2>
-                    <p className="text-gray-400 mb-4">From: {selectedEmail.senderName} &lt;{selectedEmail.email}&gt;</p>
-                    <div className="text-gray-300" dangerouslySetInnerHTML={{ __html: selectedEmail.content }} />
-                </div>
-            ) : (
-                <p className="text-gray-500">Select an email to view details</p>
-            )}
+  const { selectedEmail } = useEmailStore();
+  console.log(selectedEmail);
+
+  return (
+    <div className="flex-1 p-4">
+      {selectedEmail ? (
+        <div className=" rounded-lg p-4">
+          <h2 className="text-white text-xl font-semibold mb-2">
+            {selectedEmail.subject}
+          </h2>
+          <p className="text-gray-400 mb-4">
+            From: {selectedEmail.senderName} &lt;{selectedEmail.email}&gt;
+          </p>
+
+          <div className="p-4">
+            <div dangerouslySetInnerHTML={{ __html: selectedEmail.body.content }} />
+          </div>
         </div>
-    )
-}
+      ) : (
+        <p className="text-gray-500">Select an email to view details</p>
+      )}
+    </div>
+  );
+};
