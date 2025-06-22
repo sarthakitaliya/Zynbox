13d9aa9d493db76e87b9fe61f128d1984d6ed89b
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-06-22 17:28:34 +0530

    feat: update docker-compose service names and configure axios base URL from environment variable

--- packages/api-client/src/axiosInstance.ts
+++ packages/api-client/src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: process.env.API_BASE_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
